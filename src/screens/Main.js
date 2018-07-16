cb49b1b01c940d5c45fcef7956595ec761e1b061
lazzy07 <222lasantha@gmail.com>
2018-07-16 18:48:32 +0530

    carousal fix

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -4,7 +4,7 @@ import Carousal from '../components/carousal/Carousal.js'
 class Main extends Component {
   render() {
     return (
-      <div>
+      <div style={{paddingTop : 60}} className="col-md-8 offset-md-1">
         <Carousal/>
       </div>
     );
